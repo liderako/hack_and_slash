117a51aab7c743be30b12c73e54e3d0583732b90
Валентин Мазурок <valentin.mazurok@gmail.com>
2019-04-14 22:52:08 +0300

    inventory Ready

--- Assets/Scripts/PlayableGameObject/Player/WeaponStats.cs
+++ Assets/Scripts/PlayableGameObject/Player/WeaponStats.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class WeaponStats : MonoBehaviour
@@ -7,6 +8,8 @@ public class WeaponStats : MonoBehaviour
     public float damage;
     public float attackSpeed;
     public float cost;
+    public int id;
+    public int custom;
     public enum WeaponType
     {
         Sword,
